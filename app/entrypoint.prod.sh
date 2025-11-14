14cd2ea7c7ce44665b762aaa78c63d7f9587722f
Norbert Preining <norbert@preining.info>
2025-11-14 15:42:07 +0100

    Rework Dockerfile/entrypoint jobs
    * move some python manage.py calls into Dockerfile to speed up
      generation of files.
    * Keep compress in entrypoint.prod.sh.
    * work around the fact that we need to have static.dist on a shared
      volume in docker (compose) so that nginx can share the files.

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
+echo "============== waiting for postgres ================="
 
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
   sleep 0.1
 done
 
-echo "PostgreSQL started"
+echo "============== postgresql is ready =================="
 
-echo "============== compiling translation messages ================"
-python manage.py compilemessages
-echo "============== compiling JS translation messages ================"
-python manage.py compilejsi18n
-echo "============== running collectstatic ================"
-python manage.py collectstatic --noinput
+# Since static.dist is mounted as shared volume, we need to move over
+# the files from static.dist.backup to static.dist
+echo "============== moving files from static.dist.backup to static.dist ==="
+mv /home/app/web/eventyay/static.dist.backup/* /home/app/web/eventyay/static.dist/
+#echo "============== compiling translation messages ================"
+#python manage.py compilemessages
+#echo "============== compiling JS translation messages ================"
+#python manage.py compilejsi18n
+#echo "============== running collectstatic ================"
+#python manage.py collectstatic --noinput
 echo "============== running compress ====================="
 # collectstatic drops exe bit, but compress needs it
-chmod ugo+x /home/app/web/eventyay/static.dist/node_prefix/node_modules/rollup/dist/bin/rollup
+#chmod ugo+x /home/app/web/eventyay/static.dist/node_prefix/node_modules/rollup/dist/bin/rollup
 python manage.py compress
-#echo "============== running collectstatic a SECOND TIME ================"
-#echo "THIS SHOULD NOT BE NECESSARY, but compress creates files in static instead of static.dist"
-#python manage.py collectstatic --noinput
 echo "============== running migrate ======================"
 python manage.py migrate
-
+echo "============== starting application ================="
 exec "$@"
